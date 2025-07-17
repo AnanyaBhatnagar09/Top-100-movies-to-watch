ebc6d0657cfcaaf3e5586e6bcbdc5021c47101ae
AnanyaBhatnagar09 <166944378+AnanyaBhatnagar09@users.noreply.github.com>
2025-07-17 15:42:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
-import requests
-from bs4 import BeautifulSoup
+#Import Libraries and Load the Data
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+sns.set_style('white')
 
+# Define column names
+columns = ['user_id', 'movie_id', 'rating', 'timestamp']
 
+# Load ratings data
+ratings_df = pd.read_csv('file.tsv', sep='\t', names=columns)
 
-response = requests.get(URL)
-website_html = response.text
+# Load movie title mapping
+movies_df = pd.read_csv('Movie_Id_Titles.csv')
+movies_df.columns = ['movie_id', 'title']
 
-soup = BeautifulSoup(website_html, "html.parser")
-
-all_movies = soup.find_all(name="h3", class_="title")
-
-movie_titles = [movie.getText() for movie in all_movies]
-movies = movie_titles[::-1]
-
-with open("movies.txt", mode="w") as file:
-    for movie in movies:
-        file.write(f"{movie}\n")
-
-
+# Merge movie titles with ratings
+merged_df = pd.merge(ratings_df, movies_df, on='movie_id')
